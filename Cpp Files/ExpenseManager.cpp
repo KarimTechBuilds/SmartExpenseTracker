10ea15af3cd2f630897007f7fc06a11678fb6a07
Karim <uvswx@student.kit.edu>
2025-10-05 17:13:19 +0200

    Added removeExpense, calculateTotalExpense, and loadexpense functions to ExpenseManager class and atrributes and methods for the class.

--- Cpp Files/ExpenseManager.cpp
+++ Cpp Files/ExpenseManager.cpp
@@ -2,4 +2,56 @@
 // Created by karim on 02/10/2025.
 //
 
+
+#include <fstream>
 #include "../Header Files/ExpenseManager.h"
+
+#include <iostream>
+
+
+void ExpenseManager::calculateTotalExpense(Expense expense) {
+    totalExpense+= expense.amount;
+}
+
+void ExpenseManager::loadExpense(string targetDate) {
+    bool found=false;
+    string expenseLine;
+    ifstream expenseFile("Expense.txt");
+    expenseFile.is_open();
+    while (getline(expenseFile, expenseLine)) {
+        if (expenseLine.find(targetDate) != string::npos) {
+            found=true;
+            cout << "Expense corresponding to: "<< targetDate << "-->" << expenseLine << endl;
+        }
+    }
+    if (!found) {
+        cout << "Expense not found." << endl;
+    }
+    expenseFile.close();
+}
+
+void ExpenseManager::removeExpense(string targetDate) {
+    ifstream readFile("Expense.txt");
+    ofstream writeFile("Temp.txt");
+    string expenseLine;
+    bool deleted=false;
+    while (getline(readFile, expenseLine)) {
+        if (expenseLine.find(targetDate) == string::npos) {
+            writeFile << expenseLine << endl;
+        }
+        else {
+            deleted=true;
+        }
+    }
+    readFile.close();
+    writeFile.close();
+    remove("Expense.txt");
+    rename("Temp.txt", "Expense.txt");
+
+    if (deleted) {
+        cout << "Expense on: " << targetDate << " deleted." << endl;
+    }
+    else {
+        cout << "Expense not found." << endl;
+    }
+}
--- Header Files/ExpenseManager.h
+++ Header Files/ExpenseManager.h
@@ -1,14 +1,21 @@
 //
 // Created by karim on 02/10/2025.
 //
-
+#include "Expense.h"
 #ifndef EXPENSEMANAGER_H
 #define EXPENSEMANAGER_H
 
 
 
-class ExpenseManager {
 
+class ExpenseManager {
+public:
+    int totalExpense;
+    void calculateTotalExpense(Expense amount);
+    void loadExpense(string targetDate);
+    void editExpense();
+    void removeExpense(string targetDate);
+    void addExpense();
 };
 
 
